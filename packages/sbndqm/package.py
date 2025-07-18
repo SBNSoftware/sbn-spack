916ce78aefe5fff4bff543f377c25a9787168fe3
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2025-07-18 09:59:02 -0400

    preliminary sbndqm recipe

---
+++ packages/sbndqm/package.py
@@ -0,0 +1,74 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+def sanitize_environments(env, *vars):
+    for var in vars:
+        env.prune_duplicate_paths(var)
+        env.deprioritize_system_paths(var)
+
+class Sbndqm(CMakePackage):
+    """Main Data Quality Monitoring (DQM) package for SBN"""
+
+    homepage = "https://sbnsoftware.github.io/"
+    url = "https://github.com/SBNSoftware/sbndqm/archive/refs/tags/v1_03_00.tar.gz"
+    git = "https://github.com/SBNSoftware/sbncode.git"
+
+    version("1_03_00", sha256="06180aa009f8dde8f4864d7456170c0a6ddae9ec9c5b28881495a629a887cc81")
+    version("1_04_00")
+
+    depends_on("sbndaq-online")
+    depends_on("sbndaq-artdaq-core")
+    depends_on("sbncode")
+    depends_on("fhicl-py")
+    depends_on("fftw")
+    depends_on("cetmodules", type="build")
+
+    def url_for_version(self, version):
+        url = "https://github.com/SBNSoftware/{0}/archive/v{1}.tar.gz"
+        print("url for version: ", url.format(self.name, version.underscored))
+        return url.format(self.name, version.underscored)
+
+    def fetch_remote_versions(self, concurrency=None):
+        return dict(
+            map(
+                lambda v: (v.dotted, self.url_for_version(v)),
+                [
+                    Version(d["name"][1:])
+                    for d in sjson.load(
+                        spack.util.web.read_from_url(
+                            self.list_url, accept_content_type="application/json"
+                        )[2]
+                    )
+                    if d["name"].startswith("v")
+                ],
+            )
+        )
+    
+    def cmake_args(self):
+        args = [
+            "-DCMAKE_CXX_STANDARD={0}".format(self.spec.variants["cxxstd"].value),
+            "-DSPACK_BUILD=1"
+        ]
+        return args
+
+    def setup_run_environment(self, env):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
+
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
