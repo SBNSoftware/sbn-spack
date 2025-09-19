ab5ae2f2144bd64f5471dcb0f6aeb4113cc90c1c
Gennadiy Lukhanin <lukhanin@fnal.gov>
2025-09-19 18:32:49 -0500

    Add sbndaq  v1_11_00

--- packages/sbndaq-artdaq-core/package.py
+++ packages/sbndaq-artdaq-core/package.py
@@ -18,6 +18,7 @@ class SbndaqArtdaqCore(CMakePackage):
     git_base = "https://github.com/SBNSoftware/sbndaq-artdaq-core.git"
     list_url = "https://api.github.com/repos/SBNSoftware/sbndaq-artdaq-core/tags"
 
+    version("v1_11_00", sha256="a2c3358cdb5ddb00ab84f4706feeecf3d1ece1b31f27972c54b1454cc6f8ddc8")
     version("v1_10_09", sha256="273f94a7f406c50e3fe516a10cc9811f28ee74062a4af2dc8ba4cf0af4a5734c")
     version("v1_10_08", sha256="2dae0e0952c6005978b8f0f58cb02448805293b0a9788c59484e5da368ec0c9a")
     version("v1_10_06", sha256="45a0d2fe62226c2111992bd0b0c020b3dcdd406a3fdf13db6bdc0fb1a1e3ee40")
