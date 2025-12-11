d4914932f6ad9950260192186373f2f7a3ca018d
Your Name <you@example.com>
2025-12-11 13:15:34 -0600

    Update spack package versions and hashes

--- packages/sbndaq-artdaq/package.py
+++ packages/sbndaq-artdaq/package.py
@@ -22,7 +22,7 @@ class SbndaqArtdaq(CMakePackage):
     git_base = "https://github.com/SBNSoftware/sbndaq-artdaq.git"
     list_url = "https://api.github.com/repos/SBNSoftware/sbndaq-artdaq/tags"
 
-    version("v2_00_00", sha256="7c6d885b9acf775cef1b990ece1d94f1e82b46a81cf3e9d4ac3db03eea66b3d1")
+    version("v2_00_00", sha256="6fecefd6fe37a69ad565cd40eac15bd4f654cbdcc21c864688aa0cb84198c146")
     version("v1_10_09", sha256="4996559766ea68c1ed2a18893263dc62abb72de95c7439d4b8e4eeb450d5ed50")
     version("v1_10_08", sha256="6856af28179aee7dcfcf90d25e9662714910380fc0bc3e88e37975c245e5a6d1")
     version("v1_10_07", sha256="73cb2ca00077bc2c6465a4cf2160fc0a45c147fc1018519c56529031308c472c")
